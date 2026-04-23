d9d037980600d2483655403f1719790a404dd339
eunjin.park <ejpark78@gmail.com>
2026-04-23 13:31:24 +0900

    chore: update localpath plugin startup script for kubernetes deployment

--- docker/services/kubernetes/startup/plugins/localpath.sh
+++ docker/services/kubernetes/startup/plugins/localpath.sh
@@ -4,13 +4,10 @@
 # 노드의 로컬 디렉토리를 사용하여 동적 볼륨 프로비저닝(Dynamic PVC)을 가능하게 합니다.
 #
 
-echo "Installing Local Path Provisioner using Helm..."
-helm repo add local-path-provisioner https://rancher.github.io/local-path-provisioner
-helm repo update local-path-provisioner
+echo "Installing Local Path Provisioner using manifests..."
+kubectl apply -f https://raw.githubusercontent.com/rancher/local-path-provisioner/v0.0.35/deploy/local-path-storage.yaml
 
-helm upgrade --install local-path-provisioner local-path-provisioner/local-path-provisioner \
-    --namespace local-path-storage \
-    --create-namespace \
-    --set storageClass.defaultClass=true
+# Set as default storage class
+kubectl patch storageclass local-path -p '{"metadata": {"annotations":{"storageclass.kubernetes.io/is-default-class":"true"}}}'
 
 echo "Local Path Provisioner installation completed."
